cf063a561efb3085a191edf581979aaf85ec3d7a
meriemi <meriem.farouj@gmail.com>
2019-03-08 10:44:53 +0100

    first commit

---
+++ src/main/java/fr/univtlse3/m2dl/studentscollab/studentscollab/StudentscollabApplication.java
@@ -0,0 +1,13 @@
+package fr.univtlse3.m2dl.studentscollab.studentscollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentscollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentscollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/univtlse3/m2dl/studentscollab/studentscollab/StudentscollabApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.univtlse3.m2dl.studentscollab.studentscollab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentscollabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
