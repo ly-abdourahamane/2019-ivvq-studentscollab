90f755a8c48295d4209c651468376ab597b8941d
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-04-20 22:13:15 +0200

    Ajout de TU et Tests d'Integration de Formation

--- src/test/java/fr/univtlse3/m2dl/studentscollab/studentscollab/AllTests.java
+++ src/test/java/fr/univtlse3/m2dl/studentscollab/studentscollab/AllTests.java
@@ -1,11 +1,15 @@
 package fr.univtlse3.m2dl.studentscollab.studentscollab;
 
 import fr.univtlse3.m2dl.studentscollab.studentscollab.etudiant.*;
+import fr.univtlse3.m2dl.studentscollab.studentscollab.formation.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EtudiantControllerTests.class, EtudiantServiceTest.class, EtudiantTest.class,
-        EtudiantControllerIntegrationTest.class, EtudiantServiceIntegrationTest.class})
+        EtudiantControllerIntegrationTest.class, EtudiantServiceIntegrationTest.class,
+
+        FormationTest.class, FormationServiceTest.class, FormationControllerIntegrationTest.class, FormationServiceIntegrationTest.class
+})
 public class AllTests {
 }
